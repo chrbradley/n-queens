04822e6c5904df3da23cdd201c9e7fabf05b23f5
Chris Bradley <chrbradley (at) gmail.com>
2014-07-30 23:29:26 -0400

    solved rooks for 2x2

--- src/solvers.js
+++ src/solvers.js
@@ -14,17 +14,106 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  
+  // Set board
+  var board = new Board({'n': n});
+  // Get Rows
+  var rows = board.rows();
+  // for every row
+  for(var i = 0; i < rows.length; i++){
+  // for every square
+    for(var j = 0; j < rows[i].length; j++){
+      // place a piece
+      board.togglePiece(i, j);
+      // check for conflictss
+      if( board.hasAnyRooksConflicts() ) {
+        // if conflict, remove piece
+        board.togglePiece(i, j);
+      }
+    }
+  }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(rows));
+  // return board
+  return rows;
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+  var theMatrix = function (rounds, array) {
+    var outcomes = [];
+    //create an array of possible solutions
+    var plays = array || [ 0, 1];
+    //create a subroutine that accepts the number of rounds that have not yet been played
+    //and the running list of combos that have been played so far
+    var combos = function(roundsToGo, playedSoFar) {
+      //base case
+      if( roundsToGo === 0 ){
+        outcomes.push( playedSoFar );
+        return;
+      }
+      //for each solution in plays, create a new playedSoFar array and find all solutions
+      //that come from that branch
+      for( var i = 0; i < plays.length; i++ ){
+        var currentPlay = plays[i];
+        combos( roundsToGo-1, playedSoFar.concat(currentPlay) );
+      }
+    };
+    combos( rounds, [] );
+    return outcomes;
+  };
+  
+  // var rows = recursive call on 0,1
+  var rows = theMatrix(n);
+  // var uglyBoards = recursuve call on rows
+  var uglyBoards = theMatrix(n, rows);
+  var master = [];
+  // for every uglyBoard
+    // create empty cleanBoard array in master
+  for (var i = 0; i < uglyBoards.length; i++ ) {
+    var bucket = [];
+    //********while loop might work for this*******
+    var row1 = uglyBoards[i];
+    var row2 = row1.splice(2,2); //have to change the splice to take in n
+    bucket.push(row1, row2);
+
+    //check to make sure only one rook per row
+    var good = true;
+    // for each board 
+    for ( var j = 0; j < bucket.length; j++ ){
+      var count1 = 0;
+      var row = bucket[j];
+      // check each row
+      for (k = 0; k < row.length; k++ ){
+        // for the number of pieces
+        if( row[k] === 1 ) {
+          count1++;
+        }
+      }
+      // if there's just one piece
+      if( count1 !== 1){
+        good = false;
+      }
+    }
+    // add bucket to master
+    if(good){
+      master.push(bucket);
+    }
+      // if none increment counter
+  }
+    // push slices (rows) of uglyBoards index into cleanBoard  
+  console.log(master);
+  for(var l = 0; l < master.length; l++){
+    var board = new Board(master[l]);
+    if( board.hasAnyRooksConflicts() === false ){
+      solutionCount++;
+    }
+  }
+
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
